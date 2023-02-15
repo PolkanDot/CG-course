2bebf727dbb02cb1b2d8e89e5e2f53c9cbf03559
PolkanDot <insarmiftahov@yandex.ru>
2023-02-15 11:39:59 +0300

    Make all my initials "jump" out of sync (сode is dirty yet)

--- lw1/js/MIM.js
+++ lw1/js/MIM.js
@@ -4,25 +4,26 @@ window.onload = () => {
   tGlob = 0;
   t1 = 0;
   t2 = 0;
+  t3 = 0;
   y1 = 0;
   y2 = 0;
+  y3 = 0;
   function M1() {
-    m1.clearRect(0, 0, 800, 580);
+    m1.clearRect(0, 0, 1300, 400);
     tGlob += 1;
 
     if (tGlob >= 0)
     {
       t1 += 1;
-      y1 = 0 - 5 * t1 + (0.5 * t1 * t1) / 2;
+      y1 = 100 - 5 * t1 + (0.5 * t1 * t1) / 2;
 
-      if (y1 > 0) {
+      if (y1 > 100) {
         t1 = 0;
       }
       
-      console.log("blyat");
       m1.beginPath();
       m1.lineWidth = 2;
-      m1.strokeStyle = '#FFFFFF';
+      m1.strokeStyle = '#00000';
       m1.moveTo(10, y1 + 30);
       m1.lineTo(70, y1 + 30);
       m1.lineTo(150, y1 + 120);
@@ -41,37 +42,64 @@ window.onload = () => {
       m1.fill();
       m1.stroke();
     }      
-    
-    if (tGlob >= 10) 
-    {
+
+    if (tGlob >= 5) {
       t2 += 1;
-      y2 = 0 - 5 * t2 + (0.5 * t2 * t2) / 2;
-      if (y2 > 0) {
+      y2 = 100 - 5 * t2 + (0.5 * t2 * t2) / 2;
+      if (y2 > 100) {
         t2 = 0;
       }
 
-      console.log("hi");
       m1.beginPath();
       m1.lineWidth = 2;
-      m1.strokeStyle = '#FFFFFF';
+      m1.strokeStyle = '#00000';
       m1.moveTo(510, y2 + 30);
-      m1.lineTo(570, y2 + 30);
-      m1.lineTo(650, y2 + 120);
-      m1.lineTo(730, y2 + 30);
+      m1.lineTo(560, y2 + 30);
+      m1.lineTo(560, y2 + 115);
+      m1.lineTo(740, y2 + 30);
       m1.lineTo(790, y2 + 30);
       m1.lineTo(790, y2 + 140);
       m1.lineTo(740, y2 + 140);
       m1.lineTo(740, y2 + 60);
-      m1.lineTo(670, y2 + 140);
-      m1.lineTo(630, y2 + 140);
-      m1.lineTo(560, y2 + 60);
       m1.lineTo(560, y2 + 140);
       m1.lineTo(510, y2 + 140);
       m1.lineTo(510, y2 + 30);
-      m1.fillStyle = '#FF2041';
+      m1.fillStyle = '#1EE91B';
+      m1.fill();
+      m1.stroke();
+    }
+    
+    if (tGlob >= 10) 
+    {
+      t3 += 1;
+      y3 = 100 - 5 * t3 + (0.5 * t3 * t3) / 2;
+      if (y3 > 100) {
+        t3 = 0;
+      }
+
+      m1.beginPath();
+      m1.lineWidth = 2;
+      m1.strokeStyle = '#00000';
+      m1.moveTo(1010, y3 + 30);
+      m1.lineTo(1070, y3 + 30);
+      m1.lineTo(1150, y3 + 120);
+      m1.lineTo(1230, y3 + 30);
+      m1.lineTo(1290, y3 + 30);
+      m1.lineTo(1290, y3 + 140);
+      m1.lineTo(1240, y3 + 140);
+      m1.lineTo(1240, y3 + 60);
+      m1.lineTo(1170, y3 + 140);
+      m1.lineTo(1130, y3 + 140);
+      m1.lineTo(1060, y3 + 60);
+      m1.lineTo(1060, y3 + 140);
+      m1.lineTo(1010, y3 + 140);
+      m1.lineTo(1010, y3 + 30);
+      m1.fillStyle = '#0000FF';
       m1.fill();
       m1.stroke();
     }    
+
+
   }
   setInterval(M1, 30);
 } 
