ee8c9f3a5c9c698d910d543101eb0c065d912700
PolkanDot <insarmiftahov@yandex.ru>
2023-04-09 19:17:14 +0300

    Connect glew library

---
+++ lw3.2/OpenGL_Lab/OpenGL_Lab.cpp
@@ -0,0 +1,47 @@
+﻿#include "glew.h"
+#include "glfw3.h"
+#include <iostream>
+#define GLFW_INCLUDE_NONE
+
+void error_callback(int error, const char* description)
+{
+    fprintf(stderr, "Error: %s\n", description);
+}
+
+int main(void)
+{
+    glfwSetErrorCallback(error_callback);
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(1080, 720, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return 0;
+}
