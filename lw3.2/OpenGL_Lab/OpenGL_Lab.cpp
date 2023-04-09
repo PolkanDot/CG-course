e658091d2865f674d0bdf17f1ae1980b566f9e12
PolkanDot <insarmiftahov@yandex.ru>
2023-04-09 19:25:48 +0300

    Pasted in project a simple code with colorful and rotating triangle

--- lw3.2/OpenGL_Lab/OpenGL_Lab.cpp
+++ lw3.2/OpenGL_Lab/OpenGL_Lab.cpp
@@ -1,47 +1,75 @@
 ﻿#include "glew.h"
 #include "glfw3.h"
 #include <iostream>
-#define GLFW_INCLUDE_NONE
 
-void error_callback(int error, const char* description)
+#include <stdlib.h>
+#include <stdio.h>
+
+void error(int error, const char* description)
 {
-    fprintf(stderr, "Error: %s\n", description);
+    fputs(description, stderr);
 }
 
-int main(void)
+void key(GLFWwindow* window, int key, int scancode, int action, int mods)
 {
-    glfwSetErrorCallback(error_callback);
-    GLFWwindow* window;
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+}
 
-    /* Initialize the library */
+void display(GLFWwindow* window)
+{
+    int width, height;
+
+    glfwGetFramebufferSize(window, &width, &height);
+
+    float   ratio = width / (float)height;
+
+    glViewport(0, 0, width, height);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glOrtho(-ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+    glRotatef((float)glfwGetTime() * 50.f, 0.f, 0.f, 1.f);
+    glBegin(GL_TRIANGLES);
+    glColor3f(1.f, 0.f, 0.f);
+    glVertex3f(-0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 1.f, 0.f);
+    glVertex3f(0.6f, -0.4f, 0.f);
+    glColor3f(0.f, 0.f, 1.f);
+    glVertex3f(0.f, 0.6f, 0.f);
+    glEnd();
+}
+
+int main()
+{
+    glfwSetErrorCallback(error);
+
     if (!glfwInit())
-        return -1;
+        exit(1);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1080, 720, "Hello World", NULL, NULL);
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+
     if (!window)
     {
         glfwTerminate();
-        return -1;
+        exit(1);
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
+    glfwSwapInterval(1);
+    glfwSetKeyCallback(window, key);
 
-    /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        /* Swap front and back buffers */
+        display(window);
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
         glfwPollEvents();
     }
 
     glfwDestroyWindow(window);
     glfwTerminate();
+
     return 0;
 }
