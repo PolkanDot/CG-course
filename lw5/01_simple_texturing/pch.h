a1a82af1cd1f2afc5192e00e5bb58821d4d14239
PolkanDot <insarmiftahov@yandex.ru>
2023-05-14 15:15:52 +0300

    Added sample from teacher, but it's not working

---
+++ lw5/01_simple_texturing/pch.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#define _USE_MATH_DEFINES
+#include "glfw3.h"
+#include <chrono>
+#include <cmath>
+#include "Includes/glm/ext/matrix_clip_space.hpp"
+#include "Includes/glm/ext/matrix_transform.hpp"
+#define GLM_ENABLE_EXPERIMENTAL
+#include "Includes/glm/ext.hpp"
+
+#include <stdexcept>
